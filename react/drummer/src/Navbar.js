de1bebe3b73d32a4748775df18ec509f8a8e3889
oriolLanuza <uri22lf@gmail.com>
2020-03-31 16:27:47 +0200

    react create, using react-bootstrap

---
+++ react/drummer/src/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+export default () => (
+    <Container fluid="md">
+        <Row>
+            <Col>1 of 1</Col>
+        </Row>
+    </Container>
+)
